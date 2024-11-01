4af59b8d408b38f4b2b06ffc215e60871af48af5
susmithanunna <susmithanunna141@gmail.com>
2024-11-01 15:43:51 +0530

    firsts

--- MasterCode/src/main/java/com/example/MasterCode/Service/StudentService.java
+++ MasterCode/src/main/java/com/example/MasterCode/Service/StudentService.java
@@ -32,7 +32,7 @@ public class StudentService {
     public String updatePassword(String rollno,String password){
         Student s=studentRepository.findUser(rollno);
         if(s==null){
-            return "No user";
+            return "No user found";
         }
         studentRepository.updatePassword(rollno,password);
         return "updated";
